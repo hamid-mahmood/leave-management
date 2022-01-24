4863e4f2d6be06bd26be6f4f0fe21a4a88b37953
hamid-mahmood <hamid.mahmood96@gmail.com>
2022-01-24 13:11:12 +0500

    Add project files.

---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,53 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entry)
+        {
+            _db.LeaveAllocations.Remove(entry);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            return _db.LeaveAllocations.ToList();
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var LeaveAllocation = _db.LeaveAllocations.Find(id);
+            return LeaveAllocation;
+        }
+
+       
+
+        public bool Save()
+        {
+            return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveAllocation entry)
+        {
+            _db.LeaveAllocations.Update(entry);
+            return Save();
+        }
+    }
+}
